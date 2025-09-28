3758bfbc5ea385ceb4b92546f9aa57eb9b3154de
FiataliS <FiataliS@gmail.com>
2025-09-28 19:17:13 +0500

    add search to date and default date 3 ears old

--- src/app/photo/photos/photos.component.ts
+++ src/app/photo/photos/photos.component.ts
@@ -18,10 +18,12 @@ import { ActivatedRoute } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogComponent } from '../../dialog/dialog.component';
 import { PhotoResponse } from '../service/photo-response';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
-
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 @Component({
   selector: 'app-photos',
@@ -39,11 +41,13 @@ import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
     MatSelectModule,
     MatButtonModule,
     MatCheckboxModule,
-    DatePipe
+    DatePipe,
+    MatFormFieldModule, MatDatepickerModule
   ],
   templateUrl: './photos.component.html',
   styleUrl: './photos.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [provideNativeDateAdapter(), { provide: MAT_DATE_LOCALE, useValue: 'ru-RU' }],
 })
 export class PhotosComponent {
   phtotService = inject(PhotoService);
@@ -51,7 +55,22 @@ export class PhotosComponent {
   idTask!: string;
   readonly dialog = inject(MatDialog);
   entityes = signal<PhotoResponse[]>([]);
+  range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null)
+  });
 
+  rangeValueChanges = toSignal(this.range.valueChanges, {
+    initialValue: { start: null, end: null }
+  });
+
+  dateRangeSignal = computed(() => {
+    const rangeValues = this.rangeValueChanges();
+    return {
+      start: rangeValues.start,
+      end: rangeValues.end
+    };
+  });
 
   /////////////////////////
   selectedControl = new FormControl<'all' | 'TASK_NEW' | 'IN_PROGRESS' | 'COMPLETED'>('all');
@@ -71,6 +90,8 @@ export class PhotosComponent {
   filteredValues = computed(() => {
     const searchText = this.searchSignal()?.trim().toLowerCase();
     const filterType = this.selectedSignal();
+    const dateRange = this.dateRangeSignal();
+
     let filtered = [...this.phtotService.photos()].sort((a, b) => (b.updatedAt || 0) - (a.updatedAt || 0));
 
     switch (filterType) {
@@ -92,18 +113,46 @@ export class PhotosComponent {
       );
     }
 
+    if (dateRange.start || dateRange.end) {
+      filtered = filtered.filter(f => {
+        const photoDate = new Date(f.updatedAt);
+
+        let startCondition = true;
+        let endCondition = true;
+
+        if (dateRange.start) {
+          startCondition = photoDate >= dateRange.start;
+        }
+
+        if (dateRange.end) {
+          const endDate = new Date(dateRange.end);
+          endDate.setDate(endDate.getDate() + 1);
+          endCondition = photoDate < endDate;
+        }
+
+        return startCondition && endCondition;
+      });
+    }
+
     return filtered;
   });
 
   clearFilter() {
     this.searchControl.setValue('');
+    const today = new Date();
+    const threeYearsAgo = new Date();
+    threeYearsAgo.setFullYear(today.getFullYear() - 3);
+
+    this.range.patchValue({
+      start: threeYearsAgo,
+      end: today
+    });
   }
   /////////////////////////
 
 
   constructor(private route: ActivatedRoute) {
     effect(() => {
-      console.log(this.idTask)
       if (this.idTask) {
         this.phtotService.loadAllByTask(this.idTask);
 
@@ -121,6 +170,14 @@ export class PhotosComponent {
     if (this.phtotService.photos().length === 0) {
       this.phtotService.photos();
     }
+
+    const today = new Date();
+    const threeYearsAgo = new Date();
+    threeYearsAgo.setFullYear(today.getFullYear() - 3);
+    this.range.patchValue({
+      start: threeYearsAgo,
+      end: today
+    });
   }
 
   delete(id: string | null) {
