1eb891fbf066a6ad733d11e0c7f3b921c4324592
FiataliS <FiataliS@gmail.com>
2025-10-16 10:52:50 +0500

    add 2 button to welcome page

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-welcome',
-  imports: [],
+  imports: [MatButtonModule, RouterModule],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.css'
 })
